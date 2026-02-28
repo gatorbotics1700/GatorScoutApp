c4b09df8d8352d1506007926d1b49eefa02e3881
2027eli <emma.y.li.2008@gmail.com>
2026-02-27 17:05:57 -0800

    FRC API uses gatorbotics1700's token now, not Ayda's

--- GatorScout/FRCTeamsAPI.swift
+++ GatorScout/FRCTeamsAPI.swift
@@ -18,8 +18,8 @@ class TeamsViewModel: ObservableObject {
     func fetchTeams() {
         guard let url = URL(string: "https://frc-api.firstinspires.org/v3.0/2026/teams?eventCode=CASNV") else { return } // Silicon Valley: CASNV, East Bay: CAETB, NorCal: CANCMP
         var request = URLRequest(url: url)
-        let username = "25agokturk" // API token needs to be updated once every 3 years
-        let password = "4cd06eeb-1132-44c3-8e8a-2c0483d1a9ff"
+        let username = "gatorbotics1700" // API token needs to be updated once every 3 years, current from 2/27/26
+        let password = "ba7908be-3845-40b8-b862-8959e2357085"
         let loginString = "\(username):\(password)"
         let loginData = loginString.data(using: .utf8)!
         let base64LoginString = loginData.base64EncodedString()
